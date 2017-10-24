baec440124aa87013acd7201f1f959177a0702db
LLGZONE <985395976@qq.com>
2017-10-24 11:11:31 +0800

    add React hot loader

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import {Provider} from 'react-redux'
-import {ConnectedRouter} from 'react-router-redux'
-
+import Root from './routes/routes'
 import configureStore from './store/configureStore'
-import App from './routes'
+
 
 const store = configureStore()
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 )
