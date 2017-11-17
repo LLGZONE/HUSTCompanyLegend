1052426e629b2a09723b9766952c8f4a26876f0f
llgzone <985395976@qq.com>
2017-11-17 15:26:11 +0800

    add footer

--- src/components/managementPlatform/Company/index.js
+++ src/components/managementPlatform/Company/index.js
@@ -12,6 +12,10 @@ import PostsManage from './PostsManage'
 import PostsPublish from './PostsPublish'
 import SignUp from './SignUp'
 import TraineeFilter from './TraineeFilter'
+import Footer from '../../commons/Footer'
+
+import hotClicks from '../../Home/hotClicks.json'
+import articles from '../../Home/aticles.json'
 
 const CompanyManagement = () => {
   const prefix = '/management/company'
@@ -27,6 +31,7 @@ const CompanyManagement = () => {
       <Route path={`${prefix}perfect/message`} component={PerfectMessage}/>
       <Route path={`${prefix}/signup`} component={SignUp}/>
       <Route path={`${prefix}/trainee/filter`} component={TraineeFilter}/>
+      <Footer hotClicks={hotClicks} latestArticle={articles}/>
     </div>
   )
 }
