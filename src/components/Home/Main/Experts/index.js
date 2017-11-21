bca61fb2f9a20f4fc3746ff57feef08677fe57f1
llgzone <985395976@qq.com>
2017-11-22 01:42:34 +0800

    delete extra imgs

--- src/components/Home/Main/Experts/index.js
+++ src/components/Home/Main/Experts/index.js
@@ -2,13 +2,12 @@
  * Created by LLGZONE on 2017/10/28.
  */
 import React from 'react'
-import exps from '../images/experts.png'
 
 import './index.scss'
 
 const Experts = () => (
   <div className="home-main-experts">
-    <img src={exps} alt="专家人才" />
+
   </div>
 )
 
