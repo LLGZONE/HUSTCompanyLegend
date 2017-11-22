d8539a017960a6c39c190b269b9935d363aa9012
llgzone <985395976@qq.com>
2017-11-22 19:26:27 +0800

    update style

--- src/components/Home/Main/WorkChance/WorkDescription.js
+++ src/components/Home/Main/WorkChance/WorkDescription.js
@@ -3,16 +3,16 @@ import Clamp from 'shiitake'
 import PropTypes from 'prop-types'
 
 const WorkDescription = ({description}) => (
-  <div>
-    <Clamp lines={14} className="" overflowNode={
-      <a>{'< 查看详情 >'}</a>
+  <div className="work-chance-description">
+    <Clamp lines={14} overflowNode={
+      <a className="more-info">{'< 查看详情 >'}</a>
     }>
       {description}
-      <a>{'< 查看详情 >'}</a>
+      <a className="more-info">{'< 查看详情 >'}</a>
     </Clamp>
     <div>
-      <button>立即报名</button>
-      <button>在线咨询</button>
+      <button className="btn">立即报名</button>
+      <button className="btn">在线咨询</button>
     </div>
   </div>
 )
