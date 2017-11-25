591c89d3f3c1c29bcc28f60ab40eb20c344ea9bd
llgzone <985395976@qq.com>
2017-11-25 14:19:28 +0800

    remove file structure

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,4 +1,19 @@
 import React from 'react'
 
 import Header from '../../components/Home/Header'
-import
+import Feature from '../../components/Home/Main/Feature'
+import Announcement from '../../components/Home/Main/Announcement'
+import Members from '../../components/Home/Main/Members'
+import Achievement from '../../components/Home/Main/Achievement'
+import WorkChance from '../../components/Home/Main/WorkChance'
+import Experts from '../../components/Home/Main/Experts'
+import Recommendation from '../../components/Home/Main/Recommendation'
+import FooterHeader from '../../components/Home/Footer'
+import Footer from '../../components/commons/Footer'
+
+class Home extends React.Component {
+  constructor() {
+    super()
+
+  }
+}
